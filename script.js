086ede4aa75cd625635008bdb73861080f52967a
code-vedant <162135022+code-vedant@users.noreply.github.com>
2024-04-01 19:37:32 +0530

    page5 added

--- script.js
+++ script.js
@@ -1,45 +1,108 @@
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true,
+
+      // for tablet smooth
+      tablet: { smooth: true },
+
+      // for mobile
+      smartphone: { smooth: true }
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+          return arguments.length
+              ? locoScroll.scrollTo(value, 0, 0)
+              : locoScroll.scroll.instance.scroll.y;
+      },
+      getBoundingClientRect() {
+          return {
+              top: 0,
+              left: 0,
+              width: window.innerWidth,
+              height: window.innerHeight
+          };
+      }
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+}
+
 function cursorAnime() {
-    var page1Content = document.querySelector("#page1-content")
-var cursor = document.querySelector("#crsr");
+  var page1Content = document.querySelector("#page1-content");
+  var cursor = document.querySelector("#crsr");
 
-page1Content.addEventListener("mousemove", function(dets){
-    gsap.to(cursor,{
-        x:dets.x,
-        y:dets.y
-    })
+  page1Content.addEventListener("mousemove", function (dets) {
+    gsap.to(cursor, {
+      x: dets.x,
+      y: dets.y,
+    });
+  });
 
-})
+  page1Content.addEventListener("mouseenter", function () {
+    gsap.to(cursor, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
 
-page1Content.addEventListener("mouseenter", function(){
-    gsap.to(cursor,{
-        scale:1,
-        opacity:1
-    })
-})
-
-page1Content.addEventListener("mouseleave", function(){
-    gsap.to(cursor,{
-        scale:0,
-        opacity:0
-    })
-})
+  page1Content.addEventListener("mouseleave", function () {
+    gsap.to(cursor, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
 }
-cursorAnime()
+cursorAnime();
 
-function page2Anime(){
-    gsap.from("#left1 h3, #left2 h3, #h3",{
-        y:22,
-        stagger:0.2,
-        delay:2,
-    })
+function page2Anime() {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#top h3",
+      start: "0% 90%",
+      end: "100% 93%",
+      markers: true,
+    },
+  });
+  tl.from("#top h3", {
+    y: 22,
+    stagger: 0.2,
+  });
 
-    gsap.from("#elem h1",{
-        y:122,
-        stagger:0.1,
-        delay:2,
-        duration:0.3,
-        scrollTrigger:"#page2"
-    })
-
+  tl.from("#elem h1", {
+    y: 122,
+    stagger: 0.1,
+    duration: 0.3,
+  });
 }
-page2Anime()
+page2Anime();
+
+function page5Anime() {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#top-4 h3",
+      start: "0% 90%",
+      end: "100% 93%",
+      markers: true,
+    },
+  });
+  tl.from("#top-4 h3", {
+    y: 22,
+    stagger: 0.2,
+  });
+
+  tl.from("#elem-4 h1", {
+    y: 122,
+    stagger: 0.1,
+    duration: 0.3,
+  });
+}
+page5Anime();
+
+// locomotiveAnimation()
